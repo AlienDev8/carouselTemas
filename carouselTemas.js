9b42b9fc1cf1184b29ee5b5b12adc4fccf49ca44
Roberto Valles <kdemon.r@gmail.com>
2020-05-18 00:38:32 -0500

    Estructura html y estructura de datos

--- carouselTemas.js
+++ carouselTemas.js
@@ -18,11 +18,11 @@ var carouselTemas  = function(id,datos){
     this.html = function(){
         this.mainContainer = this.div.cloneNode(false);
             this.mainContainer.className = "container-fluid"
-            this.mainContainer.style.border = "1px solid";
+            // this.mainContainer.style.border = "1px solid";
 
         this.divEncabezado = this.div.cloneNode(false);
             this.divEncabezado.className = "row";
-            this.divEncabezado.style.border = "1px solid yellow";
+            this.divEncabezado.style.height = "40px";
             
         this.divContenido = this.div.cloneNode(false);
             this.divContenido.className = "row carousel-temas-contenido";
@@ -35,18 +35,38 @@ var carouselTemas  = function(id,datos){
 
         // crea el encabezado y sus elementos principales
         this.btnAnt = this.div.cloneNode(false);
-            this.btnAnt.className = "col-md-2 col-sm-2 col-xs-2 carousel-temas-ant";
+            this.btnAnt.className = "col-md-1 col-sm-1 col-xs-1 carousel-temas-ant";
             this.btnAnt.innerHTML = "Prev";
-            this.btnAnt.style.border = "1px solid blue";
+            this.btnAnt.style.textAlign = "center";
+            this.btnAnt.style.paddingTop = "10px";
+            this.btnAnt.style.backgroundColor = "gray";
+            this.btnAnt.style.height = "40px";
+            this.btnAnt.style.cursor = "pointer";
+            this.btnAnt.addEventListener("click", function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                alert("Antes")
+            }, false);
             
-        this.btnSig = this.btnAnt.cloneNode(false);
-            this.btnSig.className = "col-md-2 col-sm-2 col-xs-2 carousel-temas-sig";
+            this.btnSig = this.btnAnt.cloneNode(false);
+            this.btnSig.className = "col-md-1 col-sm-1 col-xs-1 carousel-temas-sig";
             this.btnSig.innerHTML = "Next";
-            this.btnSig.style.border = "1px solid blue";
+            this.btnSig.style.textAlign = "center";
+            this.btnSig.style.paddingTop = "10px";
+            this.btnSig.style.backgroundColor = "gray";
+            this.btnSig.style.height = "40px";
+             this.btnSig.style.cursor = "pointer";
+            this.btnSig.addEventListener("click", function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    alert("Siguiente")
+            }, false);
         
         this.divOpciones = this.div.cloneNode(false);
-            this.divOpciones.className = "col-md-8 col-sm-8 col-xs-8 carousel-temas-opciones"
-            this.divOpciones.style.overflowX = "scroll";
+            this.divOpciones.className = "col-md-10 col-sm-10 col-xs-10 carousel-temas-opciones"
+            // this.divOpciones.style.overflowX = "auto";
+            this.divOpciones.style.padding = "0";
+            // this.divOpciones.style.height = "40px";
 
             this.divEncabezado.append(this.btnAnt)
             this.divEncabezado.append(this.divOpciones)
@@ -64,21 +84,32 @@ var carouselTemas  = function(id,datos){
         //Encabezado opciones   
         var Ul = this.ul.cloneNode(false);
             Ul.style.padding = "0";
+            Ul.style.marginBottom = "0px";
             Ul.style.listStyle = "none";
             // Ul.style.overflowX = "hidden"
-            Ul.style.width = (tDatos * (100 + 8)) + "px"
+            Ul.style.width = (tDatos * (150 + 4)) + "px"
 
         this.divOpciones.append(Ul);
         for(var i = 0; i < tDatos; i++){
             var li = this.li.cloneNode(false);
             var divTexto = this.div.cloneNode(false);
+            divTexto.style.textAlign = "center";            
+            // divTexto.style.verticalAlign = "top"; 
+            // divTexto.style.height = "40px"; 
+            // divTexto.style.width = "150px"; 
+            divTexto.style.padding = "4px";             
+            // divTexto.style.backgroundColor = "#cdcdcd";
             divTexto.innerHTML = aDatos[i].tema
+
             li.id = "liTema_"+aDatos[i].tema_id
             li.style.display = "inline-block";
-            li.style.valing = "middle";
-            li.style.margin = "0 4px";
-            li.style.width = "100px";
-            li.style.backgroundColor = "#f1f1f1";
+            li.style.verticalAlign = "top";
+            li.style.margin = "0 2px";
+            li.style.lineHeight = "16px";
+            li.style.borderRadius = "4px";
+            li.style.width = "150px";
+            li.style.height = "40px";
+            li.style.backgroundColor = "#dcdcdc";
             li.append(divTexto);
             Ul.append(li);
         }
